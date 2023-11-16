a4be14e4ad736af3e0a3df6c13ff5d1bdef6d96f
Rajarajan <chesterking836@gmail.com>
2023-11-16 13:37:18 +0530

    added usermodel and signup api route

---
+++ api/controllers/authController.js
@@ -0,0 +1,16 @@
+import User from '../model/userModel.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export const signup = async (req, res, next) => {
+   const { username, email, password } = req.body;
+   const hashedPassword = bcrypt.hashSync(password, 10)
+   const newUser = new User({ username, email, password: hashedPassword })
+
+   try {
+        await newUser.save()
+        res.status(201).json('User is created')
+   } catch (error) {
+        next(error)
+   }
+}
--- api/index.js
+++ api/index.js
@@ -1,7 +1,33 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import authRouter from './routes/authRoutes.js'
+dotenv.config()
 
 const app = express()
+
+mongoose.connect(process.env.MONGODB)
+.then(() => {
+    console.log("Connected to MongoDB")
+})
+.catch((err) => {
+    console.log(err);
+})
+
+app.use(express.json())
+
+app.use('/api/auth', authRouter)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
+
+app.use(( err, req, res, next ) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
